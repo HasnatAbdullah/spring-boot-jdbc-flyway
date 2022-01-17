b4ca36110d6620b02c28c875ae32f7d3898a1459
Hasnat Abdullah <45420800+HasnatAbdullah@users.noreply.github.com>
2022-01-17 19:25:18 +0600

    Rowmapper added, sql formatted

--- src/main/java/com/example/demo/movie/Movie.java
+++ src/main/java/com/example/demo/movie/Movie.java
@@ -1,12 +1,13 @@
 package com.example.demo.movie;
 
 import com.example.demo.actor.Actor;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 import java.time.LocalDate;
 import java.util.List;
 
-@AllArgsConstructor
+//@AllArgsConstructor
 @Getter
 @Setter
 @ToString
@@ -16,5 +17,15 @@ public class Movie {
     private final String name;
     private final List<Actor> actors;
     private final LocalDate releaseDate;
+
+    public Movie(@JsonProperty("id")Integer id,
+                 @JsonProperty("name")String name,
+                 @JsonProperty("actors")List<Actor> actors,
+                 @JsonProperty("releaseDate")LocalDate releaseDate) {
+        this.id = id;
+        this.name = name;
+        this.actors = actors;
+        this.releaseDate = releaseDate;
+    }
 
 }
--- src/main/java/com/example/demo/movie/MovieDataAccessService.java
+++ src/main/java/com/example/demo/movie/MovieDataAccessService.java
@@ -3,31 +3,51 @@ package com.example.demo.movie;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public class MovieDataAccessService implements MovieDao {
 
+    private final JdbcTemplate jdbcTemplate;
+
+    public MovieDataAccessService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public List<Movie> selectMovies() {
-        throw new UnsupportedOperationException("not implemented");
+        String sql = "SELECT id, name, release_date FROM movie;";
+
+        return jdbcTemplate.query(sql, new MovieRowMapper());
     }
 
     @Override
     public int insertMovie(Movie movie) {
-        throw new UnsupportedOperationException("not implemented");
+        String sql = "INSERT INTO movie(name, release_date) VALUES (?, ?);";
+        return jdbcTemplate.update(
+                sql,
+                movie.getName(),
+                movie.getReleaseDate()
+        );
     }
 
     @Override
     public int deleteMovie(int id) {
-        throw new UnsupportedOperationException("not implemented");
+        String sql = "INSERT INTO movie(name, release_date) VALUES (?, ?);";
+
+        return jdbcTemplate.update(sql, id);
 
     }
 
     @Override
     public Optional<Movie> selectMovieById(int id) {
-        throw new UnsupportedOperationException("not implemented");
+        String sql = "INSERT INTO movie(name, release_date) VALUES (?, ?);";
+
+        return jdbcTemplate.query(sql, new MovieRowMapper(), id)
+                .stream()
+                .findFirst();
     }
 
 }
